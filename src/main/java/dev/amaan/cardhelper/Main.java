1fa31e02e790fd02c115b453d299b4c01346274e
tmahajan <tmahajan@redhat.com>
2019-07-23 17:35:56 +0530

    Calling webhook with HttpUrlConnection

--- src/main/java/dev/amaan/cardhelper/Main.java
+++ src/main/java/dev/amaan/cardhelper/Main.java
@@ -1,45 +1,58 @@
 package dev.amaan.cardhelper;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.amaan.cardhelper.model.Message;
-
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 public class Main {
+	public static void main(String... args) throws IOException {
+		/*
+		 * Message message = Message .messageBuilder()
+		 * .addCard().addHeader().withTitle("A").withImageUrl("B").withSubtitle("C")
+		 * .done() .addSection() .addWidget()
+		 * .addImage().withImageUrl("D").addOpenLink().withUrl("E") .done() .done()
+		 * .done() .addWidget()
+		 * .addKeyValue().withBottomLabel("F").withTopLabel("G").withContent("H")
+		 * .withContentMultiline("I") .done() .done() .done() .done() .build();
+		 */
+		/*
+		 * Message message = Message. messageBuilder() .addCard() .addSection()
+		 * .addWidget() .addImage().addOpenLink().withUrl("test.com").done().done()
+		 * .done() .done() .done() .build();
+		 */
+		Message message = Message
+				.messageBuilder()
+				.addCard()
+				.addHeader().withTitle("A").withSubtitle("C")
+				.done()
+				.done()
+				.build();
+		ObjectMapper mapper = new ObjectMapper();
 
-    public static void main(String... args) throws JsonProcessingException {
-        /*Message message = Message
-                .messageBuilder()
-                    .addCard().addHeader().withTitle("A").withImageUrl("B").withSubtitle("C")
-                              .done()
-                    .addSection()
-                        .addWidget()
-                            .addImage().withImageUrl("D").addOpenLink().withUrl("E")
-                            .done()
-                        .done()
-                    .done()
-                        .addWidget()
-                            .addKeyValue().withBottomLabel("F").withTopLabel("G").withContent("H")
-                            .withContentMultiline("I")
-                            .done()
-                        .done()
-                    .done()
-                .done()
-                .build();*/
-        Message message = Message.
-                messageBuilder()
-                .addCard()
-                .addSection()
-                .addWidget()
-                .addImage().addOpenLink().withUrl("test.com").done().done()
-                .done()
-                .done()
-                .done()
-                .build();
+		String s = mapper.writeValueAsString(message);
+		System.out.println(s);
+		// Replace the webhook with actual webhook url
+		callWebhook("webhook",s);
+	}
 
-        ObjectMapper mapper = new ObjectMapper();
+	private static void callWebhook(String webhook, String message) throws IOException {
+		URL url = new URL(webhook);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("POST");
+		conn.setRequestProperty("Content-Type", "application/json");
+		conn.setRequestProperty("accept", "application/json");
+		conn.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+		wr.writeBytes(message);
+		wr.flush();
+		wr.close();
+		if (conn.getResponseCode() != 200) {
+			System.out.println("Error occured "+conn.getResponseMessage());
+		}
 
-        String s = mapper.writeValueAsString(message);
-        System.out.println(s);
 
-    }
+		conn.disconnect();
+	}
 }
